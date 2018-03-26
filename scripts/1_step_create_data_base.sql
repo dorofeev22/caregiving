5c21dd802b89af03949cebce99f49406a7e478fc
dorofeev <dorofeev22@yandex.ru>
2018-03-26 18:03:56 +0700

    #3 add database version control

--- scripts/1_step_create_data_base.sql
+++ scripts/1_step_create_data_base.sql
@@ -2,8 +2,6 @@ CREATE DATABASE caregiving
   WITH OWNER = postgres
        ENCODING = 'UTF8'
        TABLESPACE = pg_default
-       LC_COLLATE = 'Russian_Russia.1251'
-       LC_CTYPE = 'Russian_Russia.1251'
        CONNECTION LIMIT = -1;
 \connect caregiving;
 CREATE SCHEMA caregiving;
