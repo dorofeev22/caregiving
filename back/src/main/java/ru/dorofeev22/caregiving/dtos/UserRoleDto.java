13ef5bc8b08359cb65677e6429dc8c881a644eab
dorofeev <dorofeev22@yandex.ru>
2018-04-12 18:47:33 +0700

    #21 added field "id" in UserRoleDto

--- back/src/main/java/ru/dorofeev22/caregiving/dtos/UserRoleDto.java
+++ back/src/main/java/ru/dorofeev22/caregiving/dtos/UserRoleDto.java
@@ -2,8 +2,17 @@ package ru.dorofeev22.caregiving.dtos;
 
 public class UserRoleDto {
 
+    private Long id;
     private String name;
     private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
