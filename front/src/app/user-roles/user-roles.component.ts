a8b26e1c53969d6ea173731d7d43c2f3267efaed
dorofeev <dorofeev22@yandex.ru>
2018-04-13 17:26:25 +0700

    #21 add user roles component

---
+++ front/src/app/user-roles/user-roles.component.ts
@@ -0,0 +1,35 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {CommonService} from '../common.service';
+import {UserRole} from '../domain/userRole';
+
+@Component({
+  selector: 'app-user-roles',
+  templateUrl: './user-roles.component.html',
+  styleUrls: ['./user-roles.component.css']
+})
+export class UserRolesComponent implements OnInit {
+
+  userRoles: UserRole[];
+  cols: any;
+  @Input() userRole: UserRole;
+  @Output() userRoleChange  = new EventEmitter<UserRole>();
+
+  constructor(private commonService: CommonService) {
+  }
+
+  ngOnInit() {
+    this.commonService.get('/user-role').subscribe(data => {
+      this.userRoles = <UserRole[]> data;
+    });
+    this.cols = [
+      {field: 'id', header: 'Id'},
+      {field: 'name', header: 'Role name'},
+      {field: 'description', header: 'Role description'}
+    ];
+  }
+
+  changeRole(role: UserRole) {
+    this.userRoleChange.emit(role);
+  }
+
+}
